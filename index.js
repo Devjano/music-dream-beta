f5bd3325a0fb46dc8d204de0618f6baf0f92a9a2
JANO <77622056+Devjano@users.noreply.github.com>
2022-01-03 18:09:28 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,98 @@
+const express=require("express"),app=express();app.get("/",function(e,p){p.send("Hello World")}),app.listen(3e3);
+
+const { Client, Collection } = require("discord.js"),
+  { GCommands, Color } = require("gcommands"),
+  fs = require("fs");
+
+const client = new Client();
+
+client.music = {
+  queue: {},
+  playing: {},
+  data: {}
+}
+client.modules = new Collection();
+
+/**
+ * Load Music Modules
+ */
+for (let i = 0; i < fs.readdirSync(__dirname + "/modules").length; i++) {
+  let f = fs.readdirSync(__dirname + "/modules")[i];
+  try {
+    client.modules.set(f.split(".")[0], require(`./modules/${f}`));
+    console.log(new Color("&d[Modules] &e" + f.split(".")[0] + " &aloaded").getText())
+  } catch(e) {
+    console.log(new Color("&d[Modules] &e"+f.split(".")[0]+"&c" + e).getText());
+  }
+}
+///////
+
+client.on("ready", () => {
+ client.user.setActivity(`_help`, { type: "LISTENING" });
+ 
+ });
+
+///////
+/**
+ * Guild Events
+ */
+let updateGuildQueue = (ready) => {
+  let gs = client.guilds.cache.array();
+  for (let i = 0; i < gs.length; i++) {
+    let g = gs[i];
+    if (!client.music.queue[g.id]) client.music.queue[g.id] = [];
+    if (!client.music.playing[g.id]) client.music.playing[g.id] = {};
+    if (!client.music.data[g.id]) client.music.data[g.id] = {
+      loop: false,
+      isPlaying: false,
+      djOnly: false
+    };
+
+    if (ready && g.me.voice && g.me.voice.channel) {
+      g.me.voice.setChannel(null);
+    }
+  }
+}
+
+/**
+ * Using GCommands v5
+ */
+client.on("ready", () => {
+  updateGuildQueue(true);
+  const GCommandsClient = new GCommands(client, {
+    cmdDir: "cmds/",
+    unkownCommandMessage: false,
+    language: "english",
+    slash: {
+        slash: 'both',
+        prefix: '_'
+    },
+    defaultCooldown: "3s"
+  });
+ /////
+
+  /*client.dispatcher.addInhibitor(async(cmd, {member, respond}) => {
+    let cmdBlacklist = ["help"];
+    if (cmdBlacklist.includes(cmd.name)) return true;
+
+    let djCmds = ["back","jump","leave","loop","shuffle","skip","volume","remove","clearqueue","stop","pause"];
+    let hasPermissions = await client.modules.get("CheckPermissions")(client, member, djCmds.includes(cmd.name) ? "dj" : "default");
+
+    if (!hasPermissions) {
+      respond({ content: ":x: *Insufficient permissions*", ephemeral: true });
+      return false;
+    } else {
+      return true;
+    }
+
+    return true;
+  })*/
+
+  GCommandsClient.on("debug", console.log);
+  GCommandsClient.on("log", console.log);
+});
+
+client.on("guildCreate", updateGuildQueue);
+client.on("guildDelete", updateGuildQueue);
+
+client.login("");
